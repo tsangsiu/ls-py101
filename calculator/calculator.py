a698d7a7f878745a0e34798a2c9d95d06d6dfccc
Jason Tsang <62657905+tsangsiu@users.noreply.github.com>
2025-01-14 20:29:44 +0800

    Refactor calculator.py

---
+++ calculator/calculator.py
@@ -0,0 +1,46 @@
+def prompt(message):
+    print(f'==> {message}')
+
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
+number1 = input()
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt("What's the second number?")
+number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
+prompt('What operation would you like to perform?\n'
+       '1) Add 2) Subtract 3) Multiply 4) Divide')
+operation = input()
+
+while operation not in ['1', '2', '3', '4']:
+    prompt("You must choose 1, 2, 3, or 4")
+    operation = input()
+
+match operation:
+    case '1':
+        output = int(number1) + int(number2)
+    case '2':
+        output = int(number1) - int(number2)
+    case '3':
+        output = int(number1) * int(number2)
+    case '4':
+        output = int(number1) / int(number2)
+
+prompt(f'The result is: {output}')
