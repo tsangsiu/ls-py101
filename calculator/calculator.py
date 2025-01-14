e74ef8642ad54d4a72cf85fa6ff95b1dee59b61c
Jason Tsang <62657905+tsangsiu@users.noreply.github.com>
2025-01-14 21:11:49 +0800

    Add the 'calculate again' feature to calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -9,38 +9,54 @@ def invalid_number(number_str):
 
     return False
 
+def invalid_yes_no(yes_no_str):
+    return yes_no_str.lower() not in ['n', 'y', 'no', 'yes']
+
 prompt('Welcome to Calculator!')
 
-prompt("What's the first number?")
-number1 = input()
+while True:
+  prompt("What's the first number?")
+  number1 = input()
 
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
-    number1 = input()
+  while invalid_number(number1):
+      prompt("Hmm... that doesn't look like a valid number.")
+      number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
+  prompt("What's the second number?")
+  number2 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
-    number2 = input()
+  while invalid_number(number2):
+      prompt("Hmm... that doesn't look like a valid number.")
+      number2 = input()
 
-prompt('What operation would you like to perform?\n'
-       '1) Add 2) Subtract 3) Multiply 4) Divide')
-operation = input()
+  prompt('What operation would you like to perform?\n'
+         '1) Add 2) Subtract 3) Multiply 4) Divide')
+  operation = input()
 
-while operation not in ['1', '2', '3', '4']:
-    prompt("You must choose 1, 2, 3, or 4")
-    operation = input()
+  while operation not in ['1', '2', '3', '4']:
+      prompt("You must choose 1, 2, 3, or 4")
+      operation = input()
 
-match operation:
-    case '1':
-        output = int(number1) + int(number2)
-    case '2':
-        output = int(number1) - int(number2)
-    case '3':
-        output = int(number1) * int(number2)
-    case '4':
-        output = int(number1) / int(number2)
+  match operation:
+      case '1':
+          output = int(number1) + int(number2)
+      case '2':
+          output = int(number1) - int(number2)
+      case '3':
+          output = int(number1) * int(number2)
+      case '4':
+          output = int(number1) / int(number2)
 
-prompt(f'The result is: {output}')
+  prompt(f'The result is: {output}')
+
+  prompt('Would you like to do another calculation?\n'
+         '[Y]es or [n]o?')
+  calculate_again = input()
+
+  while invalid_yes_no(calculate_again):
+      prompt("Hmm... that doesn't look like a valid answer.\n"
+             "Please enter [y]es or [n]o")
+      calculate_again = input()
+
+  if calculate_again.casefold().startswith('n'):
+      break
